afaf8b88a02bec9d0c14320d5a0b8f07e1062432
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-08-25 22:50:39 +1000

    Reformatted code to be used with UI

--- odin-rock-paper-scissors/javascript.js
+++ odin-rock-paper-scissors/javascript.js
@@ -1,7 +1,14 @@
 const choices =['Rock','Paper','Scissors']
 const display = document.getElementById("display");
-let playerWins = 0;
-let computerWins = 0;
+const player_score = document.getElementById("player-score")
+const cpu_score = document.getElementById("cpu-score")
+
+let playerScore = 0;
+let cpuScore = 0;
+player_score.innerText = playerScore;
+cpu_score.innerText = cpuScore;
+
+let gameOver = false;
 
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3); // random number 0 - 2
@@ -9,59 +16,60 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getPlayerChoice() {
-  while (true){
-    let playerChoiceInput = prompt("Enter a Number 1-3\n1: Rock\n2: Paper\n3: Scissors");
-    if (playerChoiceInput == 1 || playerChoiceInput == 2 || playerChoiceInput == 3) {
-      let playerChoice = choices[playerChoiceInput - 1]
-      return playerChoice
-    } 
+function playRound(computerChoice, playerChoice) {
+  if(gameOver == false){
+    let result;
+    // win
+    if (playerChoice == 'Rock' && computerChoice == 'Scissors' ||
+        playerChoice == 'Paper' && computerChoice == 'Rock' ||
+        playerChoice == 'Scissors' && computerChoice == 'Paper') {
+          result = "win"
+          playerScore += 1;
+          display.innerText += "Round Won!\n" + "You: " + playerChoice + " vs CPU: " + computerChoice + "\n\n";
+          player_score.innerText = playerScore
+
+
+        }
+    // lose
+    else if (playerChoice == 'Rock' && computerChoice == 'Paper' ||
+        playerChoice == 'Paper' && computerChoice == 'Scissors' ||
+        playerChoice == 'Scissors' && computerChoice == 'Rock') {
+          result = "lose"
+          cpuScore += 1
+          display.innerText += "Round lost!\n" + "You: " + playerChoice + " vs CPU: " + computerChoice + "\n\n";
+          cpu_score.innerText = cpuScore;
+        }
+    // tie
+    else if (playerChoice == computerChoice) {
+        result = "tie"
+        display.innerText += "Round tied!\n" + "You: " + playerChoice + " vs CPU: " + computerChoice + "\n\n";
+        
+      }
+
+    display.scrollTop = display.scrollHeight;     // auto scroll to the bottom of the <p>
+    checkResults()
+    return result
   }
 }
 
-function playRound(computerChoice, playerChoice) {
-  let result;
-  // win
-  if (playerChoice == 'Rock' && computerChoice == 'Scissors' ||
-      playerChoice == 'Paper' && computerChoice == 'Rock' ||
-      playerChoice == 'Scissors' && computerChoice == 'Paper') {
-        console.log("You Win!\n" + "You: " + playerChoice + " vs CPU: " + computerChoice);
-        result = "win"
-      }
-  // lose
-  else if (playerChoice == 'Rock' && computerChoice == 'Paper' ||
-      playerChoice == 'Paper' && computerChoice == 'Scissors' ||
-      playerChoice == 'Scissors' && computerChoice == 'Rock') {
-        console.log("You lose!\n" + "You: " + playerChoice + " vs CPU: " + computerChoice);
-        result = "lose"
-      }
-  // tie
-  else if (playerChoice == computerChoice) {
-      console.log("You tied!\n" + "You: " + playerChoice + " vs CPU: " + computerChoice);
-      result = "tie"
-    }
-
-  return result
+function checkResults() {
+  if(playerScore == 5){
+    gameOver = true;
+    display.innerText = "";
+    display.innerText = "You have won! Well Done\n\nPress Reset to play another game";
+  } else if (cpuScore == 5) {
+    gameOver = true;
+    display.innerText = "";
+    display.innerText = "You have lost!\n\nPress Reset to play another game";
+  }
 }
 
+function resetGame(){
+  playerScore = 0;
+  cpuScore = 0;
+  player_score.innerText = playerScore;
+  cpu_score.innerText = cpuScore;
 
-
-// function playGame(){
-//   win = 0;
-//   lose = 0;
-
-//   for (let i = 1; i <= 5; i++) {
-//     const roundResult = playRound(getComputerChoice(), getPlayerChoice())
-//     if(roundResult == "win") {
-//       playerWins++;
-//     } else if (roundResult == "lose") {
-//       computerWins++;
-//     }
-//   }
-//   console.log("Wins: " + playerWins + "\nLoses: " + computerWins);
-//   if (win > lose) {
-//     console.log("You beat the computer!\nYou won " + playerWins + " games")
-//   } else {
-//     console.log("You lost to the computer!\nYou lost " + computerWins + " games")
-//   } 
-// }
+  display.innerText = 'Winner is the first to win 5 games. Good luck!\n\n';
+  gameOver = false;
+}
