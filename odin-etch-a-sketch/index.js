c142e15cfea9fe76644bf68833e7b19a539b114a
TerminalKlone <bunyan.keiran@gmail.com>
2023-09-21 03:11:58 +1000

    Added slider & event listeners to grid

--- odin-etch-a-sketch/index.js
+++ odin-etch-a-sketch/index.js
@@ -1,17 +1,40 @@
 const display = document.getElementById("display");
 const displayWidth = document.getElementById("display").clientWidth;
-console.log(displayWidth);
+const slider = document.getElementById("gridSlider");
+const sliderOutput = document.getElementById("gridSliderOutput");
+
+let isMouseDown = false;  // track mouse state
+
+sliderOutput.innerHTML = slider.value;
+
+slider.oninput = function() {
+  sliderOutput.innerHTML = this.value;
+  document.querySelectorAll('.grid').forEach(e => e.remove());
+  createGrid(slider.value)
+
+
+}
+
+createGrid(slider.value)
 
 function createGrid(size){ 
   let gridWidth = (displayWidth / size) - 2;
 
   for (let i = 1; i <= (size*size); i++) {
     let grid = document.createElement("div");
-    grid.innerText = i;
     grid.style.width = gridWidth + "px";
-    grid.classList.add('grid');
+    grid.classList.add("grid");
+    grid.addEventListener("mousedown", function(){ isMouseDown = true; color = document.getElementById("colorChoice").value; grid.style.background = color;})
+    grid.addEventListener("mouseup", function(){ isMouseDown = false; })
+    grid.addEventListener("mouseenter", function() { changeColor(grid); });
     display.appendChild(grid);
-    
+  }
+}
+
+function changeColor(grid) {
+  if(isMouseDown){
+    color = document.getElementById("colorChoice").value;
+    grid.style.background = color;
   }
 }
 
