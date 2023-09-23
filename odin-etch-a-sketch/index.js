f742d349b93caf707a138556f0739197f8f64d57
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-09-24 01:16:46 +1000

    Added clearGrid()

--- odin-etch-a-sketch/index.js
+++ odin-etch-a-sketch/index.js
@@ -1,22 +1,25 @@
 const display = document.getElementById("display");
+const buttonBox = document.getElementById("container")
 const displayWidth = display.clientWidth;
 const displayHeight = display.clientHeight;
 const slider = document.getElementById("gridSlider");
 const sliderOutput = document.getElementById("gridSliderOutput");
 
 let isMouseDown = false;  // track mouse state
+let sliderValue = slider.value;
 
-sliderOutput.innerHTML = slider.value;
+sliderOutput.innerHTML = slider.value + " x " + slider.value;
 
 slider.oninput = function() {
-  sliderOutput.innerHTML = this.value;
-  document.querySelectorAll('.grid').forEach(e => e.remove());
-  createGrid(slider.value)
-
-
+  sliderOutput.innerHTML = this.value + " x " + this.value;
+  sliderValue = slider.value;
+  clearGrid();
 }
 
-createGrid(slider.value)
+function clearGrid() {
+  document.querySelectorAll('.grid').forEach(e => e.remove());
+  createGrid(sliderValue)
+}
 
 function createGrid(size){ 
   const cellWidth = Math.floor(displayWidth / size);
@@ -29,7 +32,6 @@ function createGrid(size){
       let grid = document.createElement("div");
       
       grid.style.width = cellWidth + "px";
-      // grid.style.height = cellHeight - 1 + "px";
       grid.classList.add("grid");
       grid.addEventListener("mousedown", function(){ isMouseDown = true; color = document.getElementById("colorChoice").value; grid.style.background = color;})
       grid.addEventListener("mouseup", function(){ isMouseDown = false; })
@@ -45,4 +47,7 @@ function changeColor(grid) {
     grid.style.background = color;
   }
 }
+
+// run this first
+createGrid(slider.value)
 
