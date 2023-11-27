2cee15a4715806bc07f24abbe99f75b740412746
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-11-27 22:49:17 +1000

    Added Subtract

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -46,7 +46,7 @@ function createNumberButtons() {
   // Add button to the screen
   numberButtonDisplay.appendChild(zeroButton)
 
-  zeroButton.addEventListener("click", function() {textDisplay.innerText += numberButton.innerText})
+  zeroButton.addEventListener("click", function() {textDisplay.innerText += zeroButton.innerText})
 
   // DECIMAL
   let decimalButton = document.createElement("button");
@@ -56,13 +56,13 @@ function createNumberButtons() {
   // Add button to the screen
   numberButtonDisplay.appendChild(decimalButton)
 
-  decimalButton.addEventListener("click", function() {textDisplay.innerText += numberButton.innerText})
+  decimalButton.addEventListener("click", function() {textDisplay.innerText += decimalButton.innerText})
 }
 
 // Create operator buttons
 function createOpereratorButtons() {
   // Create an array of operators;
-  let operators = ["C", "+", "="];
+  let operators = ["C", "+","-","="];
 
   for (let i = 0; i < operators.length; i++){
     // Create buttons
@@ -86,6 +86,10 @@ function createOpereratorButtons() {
     if (operatorButton.innerText === "+"){
       operatorButton.addEventListener("click", function() { add() })
     }
+    // SUBTRACT
+    if (operatorButton.innerText === "-"){
+      operatorButton.addEventListener("click", function() { subtract() })
+    }
   }
 }
 
@@ -93,18 +97,36 @@ function createOpereratorButtons() {
 function clearDisplay(){
   textDisplay.innerText = "";
   textHistory.innerText = "";
+  // Reset variables
+  firstNumber = undefined;
+  secondNumber = undefined;
+  result = undefined;
+  operator = undefined;
+  checker = true;
 }
 
 // Operate the equation
 function operate() {
-  secondNumber = parseFloat(textDisplay.innerText);
-  textHistory.innerText += "=";
-
+  // NOT NULL
+  if(!operator == ""){
+    secondNumber = parseFloat(textDisplay.innerText);
+    textHistory.innerText += "=";
+  }
+  // ADD
   if(operator === "+"){
     result = addResult(firstNumber, secondNumber);
     textDisplay.innerText = result;
     textHistory.innerText += result;
     checker=true;
+    operator = "";
+  }
+  // SUB
+  if(operator === "-"){
+    result = subtractResult(firstNumber, secondNumber);
+    textDisplay.innerText = result;
+    textHistory.innerText += result;
+    checker=true;
+    operator = "";
   }
   
 }
@@ -121,7 +143,20 @@ function add(){
   checker = false;
 }
 
+function subtract(){
+  // Add a checker to see if subtract was pressed last
+  if(checker){
+    // Store number
+    firstNumber = parseFloat(textDisplay.innerText);
+    operator = "-";
+    textDisplay.innerText = "";
+    textHistory.innerText += "-";
+  }
+  checker = false;
+}
+
 function addResult(a, b){ return a + b };
+function subtractResult(a,b) { return a - b};
 
 // function add(a, b){return a + b};
 
