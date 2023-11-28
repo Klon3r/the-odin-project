eded65bca30463f953ca034a3eda49da775e9113
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-11-28 23:21:45 +1000

    Cleaned up code

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -1,6 +1,5 @@
 const calculatorDisplay = document.getElementById("calc-body");
 const calculatorDisplayWidth = calculatorDisplay.clientWidth;
-
 const numberButtonDisplay = document.getElementById("number-buttons");
 const operatorButtonDisplay = document.getElementById("operator-buttons");
 const textDisplay = document.getElementById("text-display");
@@ -12,9 +11,7 @@ let result;
 let operator;
 let checker = true;
 
-// Create buttons 1 - 9
 function createNumberButtons() {
-  // Set the width/height based on the numberButtonsDisplay
   const buttonWidth = Math.floor(calculatorDisplayWidth / 4);
 
   // ONE TO NINE
@@ -23,14 +20,11 @@ function createNumberButtons() {
       // Calculate the digit
       let digit = 7 - row * 3 + col;
 
-      // Create button
       let numberButton = document.createElement("button");
       numberButton.innerText = digit;
       numberButton.style.width = buttonWidth + "px";
       numberButton.style.height = "60px"
 
-
-      // Add button to the screen
       numberButtonDisplay.appendChild(numberButton)
       
       numberButton.addEventListener("click", function() {textDisplay.innerText += numberButton.innerText})
@@ -42,8 +36,8 @@ function createNumberButtons() {
   let zeroButton = document.createElement("button");
   zeroButton.innerText = 0;
   zeroButton.style.width = (buttonWidth * 2)+ "px";
-  zeroButton.style.height = "60px"
-  // Add button to the screen
+  zeroButton.style.height = "60px";
+
   numberButtonDisplay.appendChild(zeroButton)
 
   zeroButton.addEventListener("click", function() {textDisplay.innerText += zeroButton.innerText})
@@ -53,25 +47,21 @@ function createNumberButtons() {
   decimalButton.innerText = ".";
   decimalButton.style.width = buttonWidth + "px";
   decimalButton.style.height = "60px"
-  // Add button to the screen
+
   numberButtonDisplay.appendChild(decimalButton)
 
   decimalButton.addEventListener("click", function() {textDisplay.innerText += decimalButton.innerText})
 }
 
-// Create operator buttons
 function createOpereratorButtons() {
-  // Create an array of operators;
   let operators = ["C", "+","-","="];
 
   for (let i = 0; i < operators.length; i++){
-    // Create buttons
     let operatorButton = document.createElement("button");
     operatorButton.innerText = operators[i];
     operatorButton.style.width = "60px";
     operatorButton.style.height = "60px";
 
-    // Add button to screen
     operatorButtonDisplay.appendChild(operatorButton);
 
     // CANCEL
@@ -93,11 +83,10 @@ function createOpereratorButtons() {
   }
 }
 
-// Clear the text display
 function clearDisplay(){
   textDisplay.innerText = "";
   textHistory.innerText = "";
-  // Reset variables
+  // Reset
   firstNumber = undefined;
   secondNumber = undefined;
   result = undefined;
@@ -105,7 +94,6 @@ function clearDisplay(){
   checker = true;
 }
 
-// Operate the equation
 function operate() {
   // NOT NULL
   if(!operator == ""){
@@ -132,7 +120,6 @@ function operate() {
 }
 
 function add(){
-  // Add a checker to see if add was pressed last
   if(checker){
     // Store number
     firstNumber = parseFloat(textDisplay.innerText);
@@ -144,7 +131,6 @@ function add(){
 }
 
 function subtract(){
-  // Add a checker to see if subtract was pressed last
   if(checker){
     // Store number
     firstNumber = parseFloat(textDisplay.innerText);
@@ -157,8 +143,6 @@ function subtract(){
 
 function addResult(a, b){ return a + b };
 function subtractResult(a,b) { return a - b};
-
-// function add(a, b){return a + b};
 
 createNumberButtons();
 createOpereratorButtons();
