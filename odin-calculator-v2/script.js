102164069d7b4f4a493628fb58c375d923996ddb
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-12-04 22:29:56 +1000

    Added the use of the decimal button

--- odin-calculator-v2/script.js
+++ odin-calculator-v2/script.js
@@ -5,6 +5,7 @@ let operator = "";
 let firstNumber;
 let secondNumber;
 let result;
+let canUseDecimal = true;
 
 const buttonDisplay = document.getElementById("calculator-buttons");
 const display = document.getElementById("display");
@@ -41,37 +42,50 @@ function operation(text) {
     if( text === "C") { clearDisplay(); }
     else if( text === "=") { equals(); }
     else if( text === "←") { backspace(); }
+    else if( text === ".") { decimal(); }
     else {
       display.innerText += text
       operator = text;
+      canUseDecimal = true;
     }
   }
 }
 
 function equals() {
   textArray = display.innerText.split(operator)
-  firstNumber = textArray[0];
-  secondNumber = textArray[1];
+  firstNumber = parseFloat(textArray[0]);
+  secondNumber = parseFloat(textArray[1]);
 
   if( operator === "+") { result = firstNumber + secondNumber }
   else if ( operator === "-" ) { result = firstNumber - secondNumber }
   else if ( operator === "/" ) { result = firstNumber / secondNumber }
   else if ( operator === "*" ) { result = firstNumber * secondNumber }
 
+  result = Math.round(result * 1000) / 1000;
   display.innerText = result;
+  if(result.toString().includes(".")) { canUseDecimal = false;}
 }
 
 function clearDisplay() {
   display.innerText = "";
   operationCheck = false;
+  canUseDecimal = true;
 }
 
 function backspace() {
   if(display.innerText != "") {
     let textArray = display.innerText.split("");
+    if(textArray[textArray.length-1] === "."){ canUseDecimal = true;}
     textArray.pop();
     display.innerText = textArray.join("");
   } 
+}
+
+function decimal() {
+  if(canUseDecimal === true ) { 
+    display.innerText += "."; 
+    canUseDecimal = false; 
+  }
 }
 
 createButtons();
