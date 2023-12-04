fa9cbf9a5f06213adda10c975fdca8546027519a
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-12-04 16:55:21 +1000

    Added buttonAction()

--- odin-calculator-v2/script.js
+++ odin-calculator-v2/script.js
@@ -1,6 +1,8 @@
 const buttonArray = ["C","←","+/-","/","7","8","9","+","4","5","6","-","1","2","3","*","0",".","="];
+const operators = ["C","←","+/-","/","+","-","*",".","="]
 
 const buttonDisplay = document.getElementById("calculator-buttons");
+const display = document.getElementById("display");
 const buttonDisplayWidth = buttonDisplay.clientWidth;
 const buttonDisplayHeight = buttonDisplay.clientHeight;
 
@@ -18,7 +20,12 @@ function createButtons() {
 }
 
 function buttonAction(text) {
-  alert("YOU CLICKED A BUTTON CALLED " + text);
+  if ( operators.includes(text) ) {
+    console.log("Operation: " + text)
+  } else {
+    console.log("Text: " + text);
+    display.innerText += text;
+  }
 }
 
 createButtons();
