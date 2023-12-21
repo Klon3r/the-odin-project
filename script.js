9adfea741db9e58cd4bb4a96d6704bb2106bfc6a
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-12-22 02:51:54 +1000

    Added password class and removal

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+const password= document.getElementById("password");
+const passwordConfirm = document.getElementById("confirmPassword");
+const passwordMessage = document.getElementById("passwordMessage");
+
 function addPasswordClass() {
-  
+  password.classList.add("password");
+  passwordConfirm.classList.add("password");
 }
 
 function checkPassword() {
   // get values
-  const passwordInput = document.getElementById("password").value;
-  const passwordConfirm = document.getElementById("confirmPassword").value;
+  const passwordValue = document.getElementById("password").value;
+  const passwordConfirmValue = document.getElementById("confirmPassword").value;
 
 
-  if(passwordInput === passwordConfirm) {
+  if(passwordValue === passwordConfirmValue) {
+    password.classList.remove("password");
+    passwordConfirm.classList.remove("password");
+    passwordMessage.innerText = "";
     console.log("The passwords match!")
   } else {
     console.log("The passwords do not match!")
   }
 }
+
+addPasswordClass();
+passwordMessage.innerText = "*Passwords do not match";
