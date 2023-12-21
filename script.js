6de570f2eaf8ff62ea3c5cf22f36f00ef18e1b18
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2023-12-22 03:26:09 +1000

    Cleaned up code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const password= document.getElementById("password");
 const passwordConfirm = document.getElementById("confirmPassword");
 const passwordMessage = document.getElementById("passwordMessage");
-const form = document.getElementById('form');
 const submitButton = document.getElementById('submitButton');
 var passwordsMatch = false;
 
@@ -10,33 +9,31 @@ function addPasswordClass() {
   passwordConfirm.classList.add("password");
 }
 
-function checkPassword() {
-  // get values
-  const passwordValue = document.getElementById("password").value;
-  const passwordConfirmValue = document.getElementById("confirmPassword").value;
+function removePasswordClass() {
+  password.classList.remove("password");
+  passwordConfirm.classList.remove("password");
+}
 
+function addPasswordMessage(message) { passwordMessage.innerText = message; }
+
+function checkPassword() {
+  const passwordValue = password.value;
+  const passwordConfirmValue = passwordConfirm.value;
 
   if(passwordValue === passwordConfirmValue) {
-    password.classList.remove("password");
-    passwordConfirm.classList.remove("password");
-    passwordMessage.innerText = "";
     console.log("The passwords match!")
+    removePasswordClass();
+    addPasswordMessage("");
     passwordsMatch = true;
   } else {
-    addPasswordClass()
-    passwordMessage.innerText = "*Passwords do not match";
     console.log("The passwords do not match!")
+    addPasswordClass();
+    addPasswordMessage("*Passwords do not match");
     passwordsMatch = false;
-    
   }
 }
 
-function validateForm(){
-  if(passwordsMatch === true){
-    return true;
-  } else return false;
-}
+function validateForm(){( passwordsMatch === true ) ? true : false}
 
 addPasswordClass();
-passwordMessage.innerText = "*Passwords do not match";
-// submitButton.setAttribute('disabled','disabled');
+addPasswordMessage("*Passwords do not match");
