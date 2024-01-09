09b9b382fde93f66b8c08c348ebb17bcc1f6bdb7
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-10 02:07:33 +1000

    Added functions to display, add and clear books

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const myLibrary = [];
 const content = document.getElementById("content");
 const addBookDialog = document.getElementById("addBookDialog");
-const bookNameInput = document.getElementById("bookNameInput");
-const bookAuthorInput = document.getElementById("bookAuthorInput");
-const bookPagesInput = document.getElementById("bookPagesInput");
-const bookHaveRead = document.getElementById("bookHaveRead");
 
-function Book(name, author, pages, haveRead) {
+function Book(name, author, pages, finished) {
   this.name = name;
   this.author = author;
   this.pages = pages;
-  this.haveRead = haveRead;
+  this.finished = finished;
 };
 
-function placeBooks() {
+function refreshBooks() {
   for (book in myLibrary) {
     const bookContent = document.createElement('div');
     bookContent.className = "book-content"
@@ -25,10 +21,10 @@ function placeBooks() {
     bookName.innerText = myLibrary[book].name;
     bookAuthor.innerText = myLibrary[book].author;
     bookPages.innerText = myLibrary[book].pages + " pages";
-    if(myLibrary[0].haveRead === false) { 
-      bookRead.innerText = "Not read"
+    if(myLibrary[book].finished === false) { 
+      bookRead.innerText = "Not finshed"
     } else {
-      bookRead.innerText = "Have read"
+      bookRead.innerText = "Finished"
     }
 
     bookContent.appendChild(bookName);
@@ -41,19 +37,36 @@ function placeBooks() {
 
 function addBook() {
   addBookDialog.showModal();
+  bookNameInput.value = "";
+  bookAuthorInput.value = "";
+  bookPagesInput.value = "";
+  document.getElementById('true').checked = false;
+  document.getElementById('false').checked = false;
 }
 
 function cancel() { 
   addBookDialog.close();
-  bookNameInput.value = "";
-  bookAuthorInput.value = "";
-  bookPagesInput.value = "";
-  bookHaveRead.value = "";
 };
+
+function addBookToLibrary() {
+  const bookNameInput = document.getElementById("bookNameInput");
+  const bookAuthorInput = document.getElementById("bookAuthorInput");
+  const bookPagesInput = document.getElementById("bookPagesInput");
+  const bookHaveRead = document.getElementById('true').checked;
+  const addedBook = new Book(bookNameInput.value, bookAuthorInput.value, bookPagesInput.value, bookHaveRead)
+  myLibrary.push(addedBook)
+  addBookDialog.close();
+  clearBookDisplay()
+  refreshBooks()
+}
+
+function clearBookDisplay() {
+  document.querySelectorAll('.book-content').forEach(e => e.remove());
+}
 
 // Template books
 const book1 = new Book("IT", "Stephen King", 1115, false);
-const book2 = new Book("Hannibal Rising", "Thomas Harris", 384, false)
+const book2 = new Book("Hannibal Rising", "Thomas Harris", 384, true)
 myLibrary.push(book1);
 myLibrary.push(book2);
-placeBooks();
+refreshBooks();
