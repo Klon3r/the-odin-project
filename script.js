cb2050fa19c01179474f6be2f9af99e3ed1d9bf3
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-10 00:44:34 +1000

    Added placeBooks() for displaying books on page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const content = document.getElementById("content");
 
 function Book(name, author, pages, haveRead) {
   this.name = name;
@@ -7,6 +8,40 @@ function Book(name, author, pages, haveRead) {
   this.haveRead = haveRead;
 };
 
+function placeBooks() {
+  // create div
+  // loop through myLibrary array
+  // place book onto the div
+
+  for (book in myLibrary) {
+    const bookContent = document.createElement('div');
+    bookContent.className = "book-content"
+    const bookName = document.createElement("h4");
+    const bookAuthor = document.createElement("h4");
+    const bookPages = document.createElement("h4");
+    const bookRead = document.createElement("h4");
+
+    bookName.innerText = myLibrary[book].name;
+    bookAuthor.innerText = myLibrary[book].author;
+    bookPages.innerText = myLibrary[book].pages + " pages";
+    if(myLibrary[0].haveRead === false) { 
+      bookRead.innerText = "Not read"
+    } else {
+      bookRead.innerText = "Have read"
+    }
+
+    bookContent.appendChild(bookName);
+    bookContent.appendChild(bookAuthor);
+    bookContent.appendChild(bookPages);
+    bookContent.appendChild(bookRead);
+    content.appendChild(bookContent);
+  }
+
+}
+
 // Template books
 const book1 = new Book("IT", "Stephen King", 1115, false);
 const book2 = new Book("Hannibal Rising", "Thomas Harris", 384, false)
+myLibrary.push(book1);
+myLibrary.push(book2);
+placeBooks();
