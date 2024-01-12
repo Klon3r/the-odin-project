790963b64fae5186c99de3644f61e2129077a112
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-13 00:38:37 +1000

    Added createGameBoard()

---
+++ script.js
@@ -0,0 +1,35 @@
+let gameBoard = [0,0,0,0,0,0,0,0,0] // 3 x 3
+
+// Create players
+
+// GameLogic
+
+function createGameBoard() {
+  const board = document.createElement('div');
+  const container = document.getElementById('container');
+  board.style.width = "20em";
+  board.style.height = "20em";
+  board.className = "board"
+
+  const gridAreas = [
+    '1 2 3',
+    '4 5 6',
+    '7 8 9'
+  ];
+  
+  for( let i = 1; i <= 9; i++ ) {
+    let cell = document.createElement('div');
+    cell.className = "cell-" + i;
+    cell.innerText = i;
+ 
+    cell.style.gridArea = gridAreas[Math.floor((i - 1) / 3)];
+
+    board.appendChild(cell);
+
+    cell.addEventListener('mousedown', () => {console.log(cell.className)})
+  }
+
+  container.appendChild(board);
+}
+
+createGameBoard();
