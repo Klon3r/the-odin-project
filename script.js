44bb85745a72f902d41797b02cd66af68e326029
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-14 21:42:35 +1000

    Added gameLogic to check for winner

--- script.js
+++ script.js
@@ -77,12 +77,33 @@ function handleCellClick(cell, players, gameboard) {
     // update gameboard
     gameboard.setGameboard(index, currentPlayer.symbol);
     gameboard.getGameboard();
+    gameLogic(players, gameboard, cell);
     swapPlayer(players);
     changeDisplayInfo(players);
     
   } else {
     console.log("You can't place a symbol there!")
   }
+}
+
+function gameLogic(players, gameboard) {
+  const currentPlayer = players.find(player => player.turn);
+  const currentSymbol = currentPlayer.symbol;
+  const currentCells = gameboard.getGameboard();
+
+  const winningCombinations = [
+    [0,1,2], [3,4,5], [6,7,8],  // Rows
+    [0,3,6],[1,4,7],[2,5,8],    // Columns
+    [0,4,8], [2,4,6]            // Diagonals
+  ]
+
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (currentCells[a] === currentSymbol && currentCells[b] === currentSymbol && currentCells[c] === currentSymbol) {
+      console.log(currentPlayer.name + " wins!");
+    }
+  }
+
 }
 
 function swapPlayer(players) {
