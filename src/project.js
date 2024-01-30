4855a2ebd6a5aed4f691cd8c8a9b9b3e1bf28220
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-31 01:30:25 +1000

    Added reloadProjectSidebar()

--- src/project.js
+++ src/project.js
@@ -1,3 +1,24 @@
 export function addProject(title) {
-    console.log(title);
+    localStorage.setItem(title, "")
+
+    // Add the title to project side bar
+    reloadProjectSidebar();
+}
+
+function reloadProjectSidebar() {
+    removeProjectSidebarContent();
+    const project = document.getElementById('project-title-div');
+
+    // loop through local storage
+    for (const [key] of Object.entries(localStorage)) {
+        const projectNames = document.createElement('h4');
+        projectNames.id = key;
+        projectNames.className = "project-title-list"
+        projectNames.innerText = key;
+        project.appendChild(projectNames)
+     }
+}
+
+function removeProjectSidebarContent() {
+    document.querySelectorAll('.project-title-list').forEach(e => e.remove());
 }
