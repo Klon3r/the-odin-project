d108f51b6ec83494f485812bf8c3338abc19fe38
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-31 00:18:37 +1000

    Added noteClass via class.js

---
+++ src/class.js
@@ -0,0 +1,10 @@
+export default class noteClass {
+    constructor(title, desc, dueDate, dateCreated, id, checked) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.dateCreated = dateCreated;
+        this.id = id;
+        this.checked = checked;
+    }
+}
