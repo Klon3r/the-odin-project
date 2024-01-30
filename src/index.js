5dde186e3224ee776413ddbe0b299f58281c9612
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-30 21:43:11 +1000

    Added initial function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Test")
+import css from './style.css';
+
+
+function init() {
+    const root = document.createElement('div');
+    const header = document.createElement('div');
+    const container = document.createElement('div');
+    const projects = document.createElement('div');
+    const content = document.createElement('div');
+
+    root.id = 'root';
+    container.id = 'container';
+    projects.id = 'project';
+    header.id = 'header';
+    content.id = 'content';
+    
+
+    header.innerText = 'header';
+    content.innerText = 'content';
+    projects.innerText = 'projects';
+
+
+    document.body.appendChild(root);
+    root.appendChild(header);
+    root.appendChild(container);
+    container.appendChild(projects);
+    container.appendChild(content)
+}
+
+init();
