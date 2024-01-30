6982378ec4b5ddeeae6356882e726f01c7cd65e3
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-31 01:30:09 +1000

    Added projectTitleDiv

--- src/index.js
+++ src/index.js
@@ -7,23 +7,23 @@ function init() {
     const header = document.createElement('div');
     const container = document.createElement('div');
     const project = document.createElement('div');
+    const projectTitleDiv = document.createElement('div');
     const projectButtonDiv = document.createElement('div');
     const content = document.createElement('div');
 
     root.id = 'root';
     container.id = 'container';
     project.id = 'project';
+    projectTitleDiv.id = 'project-title-div';
     projectButtonDiv.id = 'project-button-div';
     header.id = 'header';
     content.id = 'content';
     
-    content.innerText = 'content';
-    project.innerText = 'projects';
-
     document.body.appendChild(root);
     root.appendChild(header);
     root.appendChild(container);
     container.appendChild(project);
+    project.appendChild(projectTitleDiv);
     project.appendChild(projectButtonDiv);
     container.appendChild(content)
 
@@ -34,3 +34,5 @@ function init() {
 }
 
 init();
+// For testing remove localStorage
+localStorage.clear()
