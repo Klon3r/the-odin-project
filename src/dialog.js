8cd8f04aea15957452cde86e541d38d5522549a9
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-01-31 01:01:58 +1000

    Added input to dialog

--- src/dialog.js
+++ src/dialog.js
@@ -1,7 +1,11 @@
+import { addProject } from "./project";
+
 export function createProjectDialog() {
     const projectDialog = document.createElement('dialog');
     projectDialog.id = 'project-dialog'
     document.body.appendChild(projectDialog);
+
+    addInputProjectDialog()
 }
 
 export function showProjectDialog() {
@@ -9,7 +13,27 @@ export function showProjectDialog() {
     projectDialog.showModal();
 }
 
-export function closeProjectDialog() {
+function closeProjectDialog() {
     const projectDialog = document.getElementById('project-dialog');
     projectDialog.close();
+}
+
+function addInputProjectDialog() {
+    const projectDialog = document.getElementById('project-dialog');
+    const projectTitle = document.createElement('input');
+    const projectTitleLabel = document.createElement('label');
+    const projectDialogDiv = document.createElement('div');
+    const projectDialogAddButton = document.createElement('button');
+
+    projectTitle.id = 'projectTitle-input'
+    projectTitle.type = "text";
+    projectTitleLabel.textContent = "Project Name: "
+    projectDialogAddButton.innerText = "add";
+
+    projectDialogAddButton.addEventListener('click', () => { addProject( projectTitle.value ) });
+
+    projectDialog.appendChild(projectTitleLabel)
+    projectDialog.appendChild(projectTitle);
+    projectDialog.appendChild(projectDialogDiv);
+    projectDialog.appendChild(projectDialogAddButton);
 }
