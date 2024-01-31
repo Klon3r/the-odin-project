c72d1b00c15fed60ae899f3484afa166088b4000
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-02-01 01:13:03 +1000

    Added note add button & fixed bug in createNoteTitle

--- src/note.js
+++ src/note.js
@@ -1,13 +1,39 @@
-export function createNoteTitle() {
+export function noteInit() {
     const content = document.getElementById('content')
     const noteTitleDiv = document.createElement('div');
+    const noteContentDiv = document.createElement('div');
+    const noteButtonDiv = document.createElement('div');
+
+    noteTitleDiv.id = 'note-title-div';
+    noteContentDiv.id = 'note-content-div';
+    noteButtonDiv.id = 'note-button-div';
+
+    content.appendChild(noteTitleDiv);
+    content.appendChild(noteContentDiv);
+    content.appendChild(noteButtonDiv);
+    
+    
+    createNoteTitle();
+    createNoteAddButton();
+}
+
+function createNoteTitle() {
+    const noteTitleDiv = document.getElementById('note-title-div');
     const noteTitle = document.createElement('h4');
 
     noteTitle.id = 'note-title';
-    noteTitleDiv.id = 'note-title-div';
-
     noteTitle.innerText = '-Notes-';
 
-    content.appendChild(noteTitleDiv);
     noteTitleDiv.appendChild(noteTitle);
+}
+
+function createNoteAddButton() {
+    const noteButtonDiv = document.getElementById('note-button-div');
+    const noteButton = document.createElement('button');
+
+    noteButton.id = 'note-button';
+    noteButton.innerText = 'Add Note';
+
+    noteButtonDiv.appendChild(noteButton);
+
 }
