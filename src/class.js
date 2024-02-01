73c7520e830d2f9d6a616ba1cf99aa0e7a119f19
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-02-01 17:21:31 +1000

    Commented out some of the noteClass & Added setNoteToStorage function

--- src/class.js
+++ src/class.js
@@ -1,10 +1,16 @@
 export default class noteClass {
-    constructor(title, desc, dueDate, dateCreated, id, checked) {
+    constructor(title, desc) {
         this.title = title;
         this.desc = desc;
-        this.dueDate = dueDate;
-        this.dateCreated = dateCreated;
-        this.id = id;
-        this.checked = checked;
+    //     this.dueDate = dueDate;
+    //     this.dateCreated = dateCreated;
+    //     this.id = id;
+    //     this.checked = checked;
     }
+
+    setNoteToStorage(key) {
+        localStorage.setItem(key, `Title: ${this.title} Desc: ${this.desc}`)
+    }
+
+
 }
