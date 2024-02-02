a248cd6457c36921fcb80cb58b5bc7783eb374f4
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-02-02 18:59:41 +1000

    Updated webpack asset/resource

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'img/[hash][ext][query]'
     },
     module: {
         rules: [
@@ -15,9 +16,7 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
             {
-
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        
                 type: 'asset/resource',
         
             },
