0802c692ea0819a4c26b7e03af3206ae371e15a7
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-03-02 01:15:24 +1000

    Added SetupContent that add an empty weather card to page

--- src/setup.js
+++ src/setup.js
@@ -1,14 +1,18 @@
-import searchIcon from './img/search.png';
+import searchIcon from "./img/search.png";
 
 // setup
 export function SetupPage() {
   const mainDiv = document.createElement("div");
   const headerDiv = document.createElement("div");
+  const contentDiv = document.createElement('div');
 
   headerDiv.id = "header";
+  mainDiv.id = "main-div";
+  contentDiv.id = 'content-div'
 
   document.body.appendChild(mainDiv);
   mainDiv.appendChild(headerDiv);
+  mainDiv.appendChild(contentDiv);
 }
 
 export function SetupHeader() {
@@ -18,18 +22,18 @@ export function SetupHeader() {
   const rightDiv = document.createElement("div");
   const title = document.createElement("h3");
   const search = document.createElement("input");
-  const searchButton = document.createElement('img');
+  const searchButton = document.createElement("img");
 
   title.innerText = "Weather";
-  headerDiv.className = 'header-div';
-  centerDiv.className = 'header-center-div';
-  leftDiv.className = 'header-left-div';
-  rightDiv.className = 'header-right-div';
+  headerDiv.className = "header-div";
+  centerDiv.className = "header-center-div";
+  leftDiv.className = "header-left-div";
+  rightDiv.className = "header-right-div";
 
-  searchButton.className = 'search-button';
-  searchButton.src = searchIcon
+  searchButton.className = "search-button";
+  searchButton.src = searchIcon;
 
-  search.placeholder = 'Enter a city..'
+  search.placeholder = "Enter a city..";
 
   headerDiv.appendChild(leftDiv);
   leftDiv.appendChild(title);
@@ -37,4 +41,14 @@ export function SetupHeader() {
   centerDiv.appendChild(search);
   centerDiv.appendChild(searchButton);
   headerDiv.appendChild(rightDiv);
+}
+
+export function SetupContent() {
+  const contentDiv = document.getElementById("content-div");
+  const weatherCard = document.createElement("div");
+
+  contentDiv.className = "content-div";
+  weatherCard.className = "weather-card";
+
+  contentDiv.appendChild(weatherCard);
 }
