0cfa52d6824b718308c20d837a70c59035f1ecb0
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-03-05 13:24:31 +1000

    Updated function name for GetWeather()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "normalize.css";
 import css from "./style.css";
 import { SetupPage, SetupHeader, SetupContent } from "./setup";
+import { GetWeatherCurrent } from "./weather";
 
 SetupPage();
 SetupHeader();
 SetupContent();
+
+GetWeatherCurrent('brisbane');
