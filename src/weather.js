cb81ff413ccc4321a20f37a2d2b86af6ab5b76a5
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-03-05 13:50:14 +1000

    Added more variables to the weather hash map

--- src/weather.js
+++ src/weather.js
@@ -25,17 +25,25 @@ function GetWeatherCurrentInfo(weather) {
   const weather_temp_c = weather.current.temp_c;
   const weather_feelslike_c = weather.current.feelslike_c;
   const weather_humidity = weather.current.humidity;
+  const weather_last_updated = weather.current.last_updated;
+  const weather_img = weather.current.condition.icon;
+  const weather_description = weather.current.condition.text;
 
   // location info
   const location_name = weather.location.name;
   const location_region = weather.location.region;
   const location_country = weather.location.country;
 
+  const weather_last_updated_split = weather_last_updated.split(" ")
+
   // weather hashmap
   const weatherInfo = {
     temp_c: weather_temp_c,
     feelslike_c: weather_feelslike_c,
     humidity: weather_humidity,
+    last_updated: weather_last_updated_split[1],
+    description: weather_description,
+    image: weather_img,
     name: location_name,
     region: location_region,
     country: location_country,
