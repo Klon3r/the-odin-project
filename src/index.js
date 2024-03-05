339480ed57f961bab40adcf548407e4ee5a102bd
TerminalKlone <117739313+TerminalKlone@users.noreply.github.com>
2024-03-05 16:36:01 +1000

    Moved the setup functions into setup.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import "normalize.css";
 import css from "./style.css";
-import { SetupPage, SetupHeader, SetupContent } from "./setup";
+import { SetupPage } from "./setup";
 import { GetWeatherCurrent } from "./weather";
 
 SetupPage();
-SetupHeader();
-SetupContent();
-
 GetWeatherCurrent('brisbane');
