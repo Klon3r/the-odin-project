0deb9680a782567fc4f9424adf088c27f8cd8f5a
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-03 22:21:01 +1000

    Added more functions to the linked list
    - at(index)
    - pop()
    - contains(value)
    - find(value)
    - toString()

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -65,6 +65,65 @@ class LinkedList {
   tail() {
     if (this.size() >= 1) console.log("TAIL:", this.list[this.size() - 1]);
   }
+
+  /**
+   * @description Return the node at the given **index**
+   * @param {int} index
+   * @returns {object|string} The node object for the given indexm or a string if object is not found
+   */
+  at(index) {
+    if (this.list[index] === undefined) {
+      return `The linked list does not contain a node at ${index}`;
+    } else return this.list[index];
+  }
+
+  /**
+   * @description Remove the last element from the linked list
+   */
+  pop() {
+    if (this.list.length >= 1) {
+      this.list.pop();
+    } else {
+      console.log("You cannot use 'pop()' due to the list size being 0");
+    }
+  }
+
+  /**
+   * @description Returns true if the passed in value is in the list and otherwise returns false
+   * @param {int} value
+   * @returns true if the value is found otherwise returns false
+   */
+  contains(value) {
+    const found = this.list.find((element) => element.value === value);
+    if (found) return true;
+    else return false;
+  }
+
+  /**
+   *
+   * @param {int} value
+   * @returns
+   */
+  find(value) {
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].value === value) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * @description Print out the linked list in string format
+   */
+  toString() {
+    const array = [];
+    this.list.forEach((element) => {
+      array.push(`( ${element.value} ) ->`);
+    });
+    array.push("null");
+    console.log(array.join(" ").toString());
+  }
 }
 
 class Node {
@@ -74,19 +133,35 @@ class Node {
   }
 }
 
-/**
- * Add a new node containing [value] to the end of the list
- * @param {int} value
- */
-// function append(value) {
-//   const node = new Node(value, null);
-//   linkedList.addNode(node);
-// }
-
 const linkedList = new LinkedList();
-linkedList.append(10); // Old Head
+linkedList.append(10);
 linkedList.append(5);
+linkedList.append(2);
+linkedList.append(7);
+linkedList.append(9);
 linkedList.prepend(1);
+linkedList.prepend(3);
+linkedList.prepend(11);
 console.log("The amount of nodes in the list is...", linkedList.size());
 linkedList.head();
 linkedList.tail();
+const index = 2;
+console.log(`The node at index ${index}: `, linkedList.at(index));
+linkedList.pop();
+linkedList.tail();
+let contains = 5;
+console.log(
+  `Does the linked list contain ${contains}: `,
+  linkedList.contains(contains)
+);
+contains = 10;
+console.log(
+  `Does the linked list contain ${contains}: `,
+  linkedList.contains(contains)
+);
+
+console.log(
+  `Where is the value 10 in the linked list: Index `,
+  linkedList.find(10)
+);
+linkedList.toString();
