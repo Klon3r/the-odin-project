68e29853359303818626eeeb8504c8241f20d16b
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-07 02:30:50 +1000

    Added set() for the hash map
    - Added set(key, value)

--- data-structures/hash-map/HashMap.js
+++ data-structures/hash-map/HashMap.js
@@ -1,4 +1,9 @@
 export class HashMap {
+  constructor() {
+    this.hashmap = [];
+    this.bucketLength = 8;
+  }
+
   /**
    * @description Take a key and produce a hash code with it
    * @param {string} key
@@ -13,5 +18,30 @@ export class HashMap {
     }
 
     return hashCode;
+  }
+
+  /**
+   * @description Set the key/value into the hash map
+   * @param {string|int} key The key of the key value pair
+   * @param {string|int} value The value of the key value pair
+   */
+  set(key, value) {
+    const keyHash = this.hash(key);
+    const bucket = keyHash % this.bucketLength;
+
+    // Check the hashmap with the load factor and increase if needed
+    const load = this.hashmap.filter((key) => key != "");
+    if (load.length >= Math.floor(this.bucketLength * 0.75)) {
+      this.bucketLength = this.bucketLength * 2;
+      console.info("The new bucket size is...", this.bucketLength);
+    }
+
+    if (this.hashmap[bucket] !== undefined) {
+      const oldValue = this.hashmap[bucket];
+      console.error(`${oldValue} is being overwritten with ${value}`);
+      this.hashmap[bucket] = value;
+    } else {
+      this.hashmap[bucket] = value;
+    }
   }
 }
--- data-structures/hash-map/test.js
+++ data-structures/hash-map/test.js
@@ -2,4 +2,11 @@ import { HashMap } from "./HashMap.js";
 
 const test = new HashMap();
 
-console.log(test.hash("test"));
+test.set("Patrick", "Patrick Star");
+test.set("Patrick", "Patrick Star"); // Patrick Star is being overwritten with Patrick Star
+test.set("Squidward", "Squidward");
+test.set("SpongeBob", "SpongeBob SquarePants");
+test.set("Mr. Krabs", "Mr. Krabs");
+test.set("Plankton", "Plankton");
+test.set("Sandy Cheeks", "Sandy Cheeks");
+test.set("Pearl Krabs", "Pearl Krabs");
