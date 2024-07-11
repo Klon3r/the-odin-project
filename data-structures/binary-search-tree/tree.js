35d60556b2254e942d1b1a099daa8d6ea7504669
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-11 17:40:00 +1000

    Added more functions to Tree class
    - insert(val)
    - insertRecursive(node, val)
    - deleteNode(val)
    - deleteNodeRecursive(node, val)
    - minValue(node)
    - find(node, val)

--- data-structures/binary-search-tree/tree.js
+++ data-structures/binary-search-tree/tree.js
@@ -17,7 +17,7 @@ export class Tree {
     if (start > end) return null;
     let mid = Math.floor((start + end) / 2);
 
-    let root = new Node(null, null, array[mid]);
+    let root = new Node(array[mid]);
     root.setLeft(this.buildTree(array, start, mid - 1));
     root.setRight(this.buildTree(array, mid + 1, end));
 
@@ -33,5 +33,105 @@ export class Tree {
     this.sortedArray = array.toSorted((a, b) => a - b);
     this.sortedArray = Array.from(new Set(this.sortedArray));
     return this.sortedArray;
+  }
+
+  /**
+   * @description Insert value into the binary search tree
+   * @param {int} val The value to be inserted
+   */
+  insert(val) {
+    this.root = this.insertRecursive(this.root, val);
+  }
+
+  /**
+   * @description Recursively find where to insert the value
+   * @param {object} node The node object
+   * @param {int} val The value to be inserted
+   * @returns {object} A node
+   */
+  insertRecursive(node, val) {
+    if (node === null) return new Node(val);
+
+    // If value is less than, go to the left
+    if (val < node.data) {
+      node.left = this.insertRecursive(node.left, val);
+    } else if (val > node.data) {
+      // Else, go right
+      node.right = this.insertRecursive(node.right, val);
+    } else if (val === node.data) {
+      console.error("The value already exists in the binary search tree");
+      return node; // Return the existing node
+    }
+
+    return node;
+  }
+
+  /**
+   * @description Delete a node in the binary search tree
+   * @param {int} val The value to find and delete
+   */
+  deleteNode(val) {
+    this.root = this.deleteNodeRecursive(this.root, val);
+  }
+
+  /**
+   * @description The recursive function for delete nodej
+   * @param {object} node The node object
+   * @param {int} val The value to delete
+   * @returns {object} Returns a node
+   */
+  deleteNodeRecursive(node, val) {
+    if (node === null) return null;
+
+    if (val < node.data) {
+      node.left = this.deleteNodeRecursive(node.left, val);
+    } else if (val > node.data) {
+      node.right = this.deleteNodeRecursive(node.right, val);
+    } else {
+      console.warn("Value Found:", val);
+
+      // Check there is any children
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+
+      node.data = this.minValue(node.right);
+      node.right = this.deleteNodeRecursive(node.right, node.data);
+    }
+    return node;
+  }
+
+  /**
+   * Finds and returns the minimum value in a binary search tree (BST)
+   * @param {object} node The node
+   * @returns  {int} The minimum value found in the BST
+   */
+  minValue(node) {
+    let current = node;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  /**
+   * @description Find the node and when found return the node & its children
+   * @param {object} node The node
+   * @param {int} val The value to find
+   * @returns {object} Returns the node object
+   */
+  find(node, val) {
+    if (node === null) return null;
+
+    if (val > node.data) {
+      return this.find(node.right, val);
+    } else if (val < node.data) {
+      return this.find(node.left, val);
+    } else {
+      //console.log(node);
+      return node;
+    }
   }
 }
