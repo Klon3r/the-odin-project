b18bc793d4b354d895007154124f220314577590
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-18 01:07:57 +1000

    Added canVisit array onto graph edges

--- data-structures/knights-travails/test.js
+++ data-structures/knights-travails/test.js
@@ -1,4 +1,5 @@
 import { Board } from "./board.js";
+import { generateKnightsMoves } from "./knight.js";
 
 let test = new Board();
 
@@ -9,4 +10,13 @@ for (let row = 0; row < 8; row++) {
   }
 }
 
+// Grab the possible knight moves and put then into a graph
+const canVisit = generateKnightsMoves();
+for (const [key, neighbours] of canVisit) {
+  for (const [x, y] of neighbours) {
+    const keySplit = key.split(",");
+    test.addEdge(keySplit[0], keySplit[1], x, y);
+  }
+}
+//console.log(canVisit);
 test.print();
