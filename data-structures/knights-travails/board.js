9a95512beefe34a6f54c814dd13fe1b5c953adab
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-17 23:19:29 +1000

    Added JSDoc commenting for functions

--- data-structures/knights-travails/board.js
+++ data-structures/knights-travails/board.js
@@ -3,14 +3,26 @@ export class Board {
     this.adjacencyList = new Map();
   }
 
+  /**
+   * @description Add a node using x-coord & y-coord
+   * @param {int} x x-coordinate
+   * @param {int} y y-coordinate
+   */
   addNode(x, y) {
     const key = `${x},${y}`;
     this.adjacencyList.set(key, []);
   }
 
-  addEdge(x, y, vX, vY) {
+  /**
+   * @description Add an edge between chess board location & where you can travel too
+   * @param {int} x x-coordinate
+   * @param {int} y y-coordinate
+   * @param {int} visitX x-coordinate you can visit
+   * @param {int} visitY y-coordinate you can visit
+   */
+  addEdge(x, y, visitX, visitY) {
     const key = `${x},${y}`;
-    const key2 = `${vX},${vY}`;
+    const key2 = `${visitX},${visitY}`;
     this.adjacencyList.get(key).push(key2);
     this.adjacencyList.get(key2).push(key);
   }
