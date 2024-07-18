340ad04d291146443ae3e75e0ba2d81c8eea9449
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-19 01:27:47 +1000

    Added knightMoves()

--- data-structures/knights-travails/run.js
+++ data-structures/knights-travails/run.js
@@ -11,12 +11,44 @@ for (let row = 0; row < 8; row++) {
 }
 
 // Grab the possible knight moves and put then into a graph
-const canVisit = generateKnightsMoves();
-for (const [key, neighbours] of canVisit) {
+const canVisitArray = generateKnightsMoves();
+const canVisit = new Map(canVisitArray);
+for (const [key, neighbours] of canVisit.entries()) {
   for (const [x, y] of neighbours) {
-    const keySplit = key.split(",");
-    test.addEdge(keySplit[0], keySplit[1], x, y);
+    const [keyX, keyY] = key.split(",").map(Number);
+    test.addEdge(keyX, keyY, x, y);
   }
 }
-//console.log(canVisit);
-test.print();
+
+function knightMoves(start, end) {
+  const [endX, endY] = end;
+
+  const queue = [[start]];
+  const visited = new Set();
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    let path = queue.shift();
+    let [currentX, currentY] = path[path.length - 1];
+
+    if (currentX === endX && currentY === endY) {
+      return path;
+    }
+
+    const key = `${currentX},${currentY}`;
+    if (canVisit.has(key)) {
+      for (const [nextX, nextY] of canVisit.get(key)) {
+        const nextPos = [nextX, nextY];
+        if (!visited.has(nextPos.toString())) {
+          visited.add(nextPos.toString());
+          queue.push([...path, nextPos]);
+        }
+      }
+    }
+  }
+
+  // Return an empty array if no path is found
+  return [];
+}
+
+console.log(knightMoves([0, 0], [7, 7])); //EXPECT: [[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]
