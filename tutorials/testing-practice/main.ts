bb4ff952459a740054cbc169a7f47aefc2b449b8
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-07-23 20:30:45 +1000

    Removed testing comment

--- tutorials/testing-practice/main.ts
+++ tutorials/testing-practice/main.ts
@@ -160,15 +160,4 @@ export function analyzeArray(array: number[]) {
 
     return object;
   }
-
-  /**
-  
-    
-    [1,8,3,4,2,6
-   object == {
-   average: 4,
-   min: 1,
-   max: 8,
-   length: 6
-   **/
 }
